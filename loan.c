8cd1722d39e04577fa8288c3542e1db4433bb692
AlphaGithinji <githinjialpha846@gmail.com>
2024-10-02 09:02:15 +0100

    loan.c

--- loan.c
+++ loan.c
@@ -1,3 +1,10 @@
+//Progrm to check loan eligibility
+/*
+Author:Alpha Githinji
+Reg no:CT102/G/24626/24
+Date:28/9/24
+	*/
+
 #include <stdio.h>
 int main(){
 
@@ -12,6 +19,11 @@ int main(){
     scanf("%lf", &income);
     
     if(age >= 21 && income >= 21000) {printf("Congratulations, you qualify for a loan.\n");} 
+
+    else{printf("Unfortunately,we are unable to offer you a loan at this time.\n);}
+
+	    return 0;
+	}
     	
 	else { printf("Unfortunately, we are unable to offer you a loan at this time.\n");}
 
