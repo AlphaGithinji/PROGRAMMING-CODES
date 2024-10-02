a6779b78f4e1952c710112739a1094aa04f8351c
AlphaGithinji <githinjialpha846@gmail.com>
2024-10-02 09:04:47 +0100

    loan.c

--- loan.c
+++ loan.c
@@ -20,7 +20,7 @@ int main(){
     
     if(age >= 21 && income >= 21000) {printf("Congratulations, you qualify for a loan.\n");} 
 
-    else{printf("Unfortunately,we are unable to offer you a loan at this time.\n);}
+    else{printf("Unfortunately,we are unable to offer you a loan at this time.\n");}
 
  return 0;
 }
